1937d68244007bb051dcdd6d5a8b5f6281ae0e64
GADDI Rishab <rgaddi@murex.com>
2025-04-30 16:33:12 +0200

    refactor

--- bank/src/test/java/com/bank/AccountTest.java
+++ bank/src/test/java/com/bank/AccountTest.java
@@ -7,30 +7,32 @@ import static org.junit.Assert.assertThrows;
 
 public class AccountTest {
 
+    public static final String ACCOUNT_NUMBER = "123456789";
+
     @Test
     public void account_creation() {
-        Account account = new Account("123456789");
+        Account account = new Account(ACCOUNT_NUMBER);
         assertEquals(0.0, account.getBalance(), 0.0);
-        assertEquals("123456789", account.getAccountNumber());
+        assertEquals(ACCOUNT_NUMBER, account.getAccountNumber());
     }
 
     @Test
     public void client_can_deposit() {
-        Account account = new Account("123456789");
+        Account account = new Account(ACCOUNT_NUMBER);
         account.deposit(100.0);
         assertEquals(100.0, account.getBalance(), 0.01);
     }
 
     @Test
     public void negative_deposit_does_not_change_balance() {
-        Account account = new Account("123456789");
+        Account account = new Account(ACCOUNT_NUMBER);
         account.deposit(-50.0);
         assertEquals(0.0, account.getBalance(), 0.01);
     }
 
     @Test
     public void client_can_withdraw() {
-        Account account = new Account("123456789");
+        Account account = new Account(ACCOUNT_NUMBER);
         account.deposit(200.0);
         double amountWithdrawn = account.withdraw(100.0);
         assertEquals(100.0, account.getBalance(), 0.01);
@@ -39,7 +41,7 @@ public class AccountTest {
 
     @Test
     public void client_cannot_withdraw_more_than_balance() {
-        Account account = new Account("123456789");
+        Account account = new Account(ACCOUNT_NUMBER);
         account.deposit(100.0);
         IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> account.withdraw(200.0));
         assertEquals("Insufficient funds", illegalArgumentException.getMessage());
