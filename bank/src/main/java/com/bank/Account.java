b1cb93efb40788b78feed8711ad37ba5d0848a03
GADDI Rishab <rgaddi@murex.com>
2025-04-30 15:06:17 +0200

    Client can deposit the amount in the account

--- bank/src/main/java/com/bank/Account.java
+++ bank/src/main/java/com/bank/Account.java
@@ -1,4 +1,25 @@
 package com.bank;
 
-public record Account(double balance, String accountNumber) {
+public class Account {
+
+    private final String accountNumber;
+
+    private double balance;
+
+    public Account(String accountNumber) {
+        this.balance = 0.0;
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
 }
--- bank/src/test/java/com/bank/AccountTest.java
+++ bank/src/test/java/com/bank/AccountTest.java
@@ -8,9 +8,16 @@ public class AccountTest {
 
     @Test
     public void account_creation() {
-        Account account = new Account(99.99, "123456789");
-        assertEquals(99.99, account.balance(), 0.01);
-        assertEquals("123456789", account.accountNumber());
+        Account account = new Account( "123456789");
+        assertEquals(0.0, account.getBalance(), 0.0);
+        assertEquals("123456789", account.getAccountNumber());
+    }
+
+    @Test
+    public void client_can_deposit() {
+        Account account = new Account("123456789");
+        account.deposit( 100.0);
+        assertEquals(100.0, account.getBalance(), 0.01);
     }
 
 }
